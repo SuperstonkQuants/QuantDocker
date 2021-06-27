da3aa7393b2c2095e159eb6cd0a4b29f9d2fe1d8
FireSpark142 <elfboy5500@gmail.com>
2021-06-27 10:22:27 -0500

    Remove MLFlow docker, too chonky

--- mlflow-master/mlflow-master/mlflow/server/js/src/experiment-tracking/constants.js
+++
@@ -1,7 +0,0 @@
-export const ColumnTypes = {
-  ATTRIBUTES: 'attributes',
-  PARAMS: 'params',
-  METRICS: 'metrics',
-  TAGS: 'tags',
-};
-export const MLMODEL_FILE_NAME = 'MLmodel';
--- mlflow-master/mlflow/server/js/src/common/utils/StringUtils.js
+++
@@ -1,8 +0,0 @@
-import _ from 'lodash';
-
-export const truncateToFirstLineWithMaxLength = (str, maxLength) => {
-  const truncated = _.truncate(str, {
-    length: maxLength,
-  });
-  return _.takeWhile(truncated, (char) => char !== '\n').join('');
-};
